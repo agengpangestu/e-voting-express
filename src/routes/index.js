3b82aacca8e29e6ab75dcbffe6d31970a35248e9
Ageng Pangestu <agengp617@gmail.com>
2024-03-28 17:24:51 +0700

    add route candidate

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const UserRouter = require('./user.route');
+const CandidateRouter = require('./candidate.route');
 
 router.use('/users', UserRouter);
+router.use('/candidates', CandidateRouter);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
