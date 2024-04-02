865ede129fbc3312b75e5bd8d8cb5b73696d7978
Ageng Pangestu <agengp617@gmail.com>
2024-04-02 14:03:36 +0700

    add route schedule

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const UserRouter = require('./user.route');
 const CandidateRouter = require('./candidate.route');
+const ScheduleRouter = require('./schedule.route');
 
 router.use('/users', UserRouter);
 router.use('/candidates', CandidateRouter);
+router.use('/schedules', ScheduleRouter);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
