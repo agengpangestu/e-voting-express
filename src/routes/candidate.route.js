3c9ca72500c458cbc3b0607a25653d322e11e72c
Ageng Pangestu <agengp617@gmail.com>
2024-04-02 14:01:53 +0700

    add check created candidate in route

--- src/routes/candidate.route.js
+++ src/routes/candidate.route.js
@@ -1,11 +1,21 @@
 const CandidateController = require('../controllers/candidate.controller');
+const CandidateHandler = require('../middlewares/candidate.handler');
 
 const router = require('express').Router();
 
 router.get('/', CandidateController.Get);
+
 router.get('/:id/candidate', CandidateController.GetByID);
-router.post('/post', CandidateController.Post);
-router.patch('/:id/candidate-update', CandidateController.Update);
+
+router.post('/post',
+    CandidateHandler.CheckWhoCreated,
+    CandidateHandler.CandidateBodyRequired,
+    CandidateController.Post);
+
+router.patch('/:id/candidate-update',
+    CandidateHandler.CheckWhoCreated,
+    CandidateController.Update);
+
 router.delete('/:id/candidate-delete', CandidateController.Deleted);
 
 module.exports = router;
