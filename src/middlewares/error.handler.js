c9454547c296f876da347db466c9dec350b2120b
Ageng Pangestu <agengp617@gmail.com>
2024-05-26 11:37:06 +0700

    add validation prisma error

---
+++ src/middlewares/error.handler.js
@@ -0,0 +1,68 @@
+const { Prisma } = require("@prisma/client");
+const multer = require("multer");
+
+const ErrorHandler = (err, req, res, next) => {
+
+    if (err instanceof Prisma.PrismaClientValidationError) {
+        return res.status(400).json({
+            name: err.name,
+            messsage: `Validation Error: ${err.message}`,
+            stack: err.stack
+        });
+    }
+
+    if (err instanceof Prisma.PrismaClientInitializationError)
+        return res.status(err.errorCode).json({
+            name: err.name,
+            message: `Initialization Error: ${err.message}`,
+            stack: err.stack,
+            retryable: err.retryable
+
+        })
+
+    if (err instanceof Prisma.PrismaClientKnownRequestError)
+        res.status(400).json({
+            name: err.name,
+            message: `Know Req Error: ${err.message}`,
+            stack: err.stack,
+            meta: err.meta
+        })
+
+    if (err instanceof Prisma.PrismaClientRustPanicError)
+        res.status(400).json({
+            name: err.name,
+            message: `Rust Panic Error: ${err.message}`,
+            stack: err.stack
+        })
+
+    if (err instanceof Prisma.PrismaClientUnknownRequestError)
+        res.status(400).json({
+            name: err.name,
+            message: `Unknow Req Error: ${err.message}`,
+            stack: err.stack
+        })
+
+    if (err instanceof multer.MulterError) {
+        return res
+            .status(err.code)
+            .json({
+                name: err.name,
+                message: err.message,
+                stack: err.stack
+            })
+    }
+
+    const statusCode = err.statusCode || 500;
+    const errMsg = err.messsage || "Something went wrong";
+
+    return res
+        .status(statusCode)
+        .json({
+            success: false,
+            status: statusCode,
+            messsage: errMsg,
+            stack: process.env.NODE_ENV === 'development' ? err.stack : {}
+        });
+};
+
+module.exports = ErrorHandler;
