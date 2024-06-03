0d69283c5b5756d22f7de8afc9bef62c22c72da2
Ageng Pangestu <agengp617@gmail.com>
2024-06-03 14:10:05 +0700

    make router & add handler to route post,get

---
+++ src/routes/identity.verif.route.js
@@ -0,0 +1,23 @@
+const identityVerifController = require('../controllers/identity.verif.controller');
+const identityVerifHandler = require('../middlewares/identity.verif.handler');
+
+const router = require('express').Router();
+
+router.get('/', identityVerifController.Get);
+router.post('/post',
+    identityVerifHandler.checkNoKTP,
+    identityVerifHandler.checkNISN,
+    identityVerifHandler.checkNoAnggota,
+    identityVerifController.Post);
+router.get('/:id',
+    identityVerifHandler.checkData,
+    identityVerifController.GetByID);
+router.patch('/:id/identity-update',
+    identityVerifHandler.checkData,
+    identityVerifController.Update);
+router.delete('/:id/identity-deleted',
+    identityVerifHandler.checkData,
+    identityVerifController.Deleted);
+
+
+module.exports = router;
