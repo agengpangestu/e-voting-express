1072343eabb51189c549bb56748f576a43e1cc82
Ageng Pangestu <agengp617@gmail.com>
2024-06-05 23:09:36 +0700

    fix static file & parser json

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./src/routes');
-var usersRouter = require('./src/routes/users');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var app = express();
+const indexRouter = require('./src/routes');
+const usersRouter = require('./src/routes/users');
+const ErrorHandler = require('./src/middlewares/error.handler');
+
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views')); // must src/views if in directory src
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(cors('*'))
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public',express.static(path.join(__dirname, 'public'))); // must src/public if in directory src
 
-app.use('/', indexRouter);
+app.use('/api', indexRouter, ErrorHandler);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
