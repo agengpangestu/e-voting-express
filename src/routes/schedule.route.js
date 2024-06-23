9d4f384d0c34bb93d2719c58630bfb39b0301f1d
Ageng Pangestu <agengp617@gmail.com>
2024-06-23 21:51:35 +0700

    added handler to schedule router

--- src/routes/schedule.route.js
+++ src/routes/schedule.route.js
@@ -1,10 +1,14 @@
 const ScheduleController = require('../controllers/schedule.controller');
+const ScheduleHandler = require('../middlewares/schedule.handler');
 
 const router = require('express').Router();
 
 router.get('/', ScheduleController.Get);
 router.get('/:id/schedule', ScheduleController.GetByID);
-router.post('/post', ScheduleController.Post);
+router.post('/post',
+    ScheduleHandler.CheckWhoCreated,
+    ScheduleController.Post
+);
 router.patch('/:id/schedule-update', ScheduleController.Update);
 router.delete('/:id/schedule-delete', ScheduleController.Deleted);
 
