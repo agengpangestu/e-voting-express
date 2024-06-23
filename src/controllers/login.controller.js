aa32117ad4de88c77f6911e779ec51f5e1a0a55e
Ageng Pangestu <agengp617@gmail.com>
2024-06-23 21:46:57 +0700

    add validation & send data user when login

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -8,15 +8,26 @@ class LoginController {
     async login(req, res, next) {
         const { username, password } = req.body;
 
-        await User.findUnique({ where: { username: username } })
+        if (!username && !password) return res.status(400).json({ message: "Isi username dan password!", status: 400 });
+        else if (!username) return res.status(400).json({ message: "Isi username!", status: 400 });
+        else if (!password) return res.status(400).json({ message: "Isi password!", status: 400 });
+
+        else await User.findUnique({ where: { username: username } })
             .then(async (retrieved) => {
                 (!retrieved)
                     ? res.status(404).json({ message: "Username tidak ditemukan", status: 404 })
                     : await bcrypt.compare(password, retrieved.password).then((ifMatching) => {
                         if (!ifMatching) return res.status(404).json({ message: "Password salah", status: 400 });
 
+                        let data = {
+                            id: retrieved.id,
+                            name: retrieved.fullName,
+                            role: retrieved.role,
+                            email: retrieved.email
+                        };
+
                         const token = generateToken(retrieved);
-                        res.status(200).json({ message: "Log in success", token: token })
+                        res.status(200).json({ message: "Log in success", token: token, data: data })
 
                     })
             }).catch((err) => {
