a3159d259d5f30fa13650b81586185a9ada5e042
Ageng Pangestu <agengp617@gmail.com>
2024-06-23 21:46:10 +0700

    create route for user & fix post and patch

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,60 @@
+const { faker, fa } = require('@faker-js/faker');
+
 const UserController = require('../controllers/user.controller');
+const prisma = require('../database/prisma');
+const { uploadIdentityCards } = require('../middlewares/multer.handler');
+const userHandler = require('../middlewares/user.handler');
 
 const router = require('express').Router();
 
+const bulkUsers = () => {
+    return {
+        username: faker.internet.userName(),
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        role: "PEMILIH",
+        fullName: faker.person.fullName(),
+        age: 20,
+    }
+};
+
+const createRandomUsers = async (count) => {
+    const userData = Array.from({ length: count }, () => bulkUsers());
+    const createdUsers = await prisma.user.createMany({
+        data: userData.map(user => ({
+            username: user.username,
+            password: user.password,
+            email: user.email,
+            role: user.role,
+            fullName: user.fullName,
+            age: user.age,
+        }))
+    });
+    return createdUsers;
+};
+
 router.get('/', UserController.Get);
 router.get('/:id/user', UserController.GetByID);
-router.post('/post', UserController.Post);
-router.patch('/:id/user-update', UserController.Update);
+router.post('/post',
+    uploadIdentityCards.single('identityPicture'),
+    userHandler.checkInputData,
+    userHandler.checkUsername,
+    UserController.Post
+);
+router.post('/post-bulk', async (req, res) => {
+    try {
+        const { count } = req.body;
+        const createdUsers = await createRandomUsers(count);
+        res.json({ message: "OK", data: createdUsers });
+    } catch (error) {
+        console.log(error);
+        res.json({ message: error })
+    }
+});
+router.patch('/:id/user-update', 
+    uploadIdentityCards.single('identityPicture'),
+    UserController.Update
+);
 router.delete('/:id/user-delete', UserController.Deleted);
 
 module.exports = router;
